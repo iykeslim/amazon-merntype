91123ae0c3d94c7db76612dc416bcca954e82851
iykeslim <iykeslim01@gmail.com>
2022-03-05 00:58:03 +0100

    backend changes

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+})
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Served at http://localhost:5000`);
+})
